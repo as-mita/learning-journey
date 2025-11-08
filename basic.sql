2a6fc17dfa71509eac36c201d6de0bd0b661481c
as-mita <atimsa60@gmail.com>
2025-11-08 22:08:07 +0545

    Add

---
+++ basic.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE students (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    grade CHAR(2)
+);
+
+INSERT INTO students VALUES (1, 'Asmita', 22, 'A');
+
+SELECT * FROM students;
